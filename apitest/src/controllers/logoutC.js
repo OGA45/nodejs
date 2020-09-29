3691ad9d9deea92339eab90de503ab26ac888e33
OGA45 <50638966+OGA45@users.noreply.github.com>
2020-09-30 07:08:53 +0900

    Add files via upload

--- apitest/src/controllers/logoutC.js
+++ apitest/src/controllers/logoutC.js
@@ -1,20 +1,13 @@
 require('mongoose');
 const jwt = require('jsonwebtoken');
+const G=require('./token_get')
 const F=require('./token_check')
 const Token=require('../model/token_model');
 const { exit } = require('process');
 exports.LogoutF=async(req,res,next)=>{
-    try {
-        var token=req.headers.authorization.split(' ')[1]; //ヘッダーから取ってくる
-    }catch(err){//無ければエラー
-        console.log(err.name);
-          return res.json({
-            success: false,
-            msg: 'トークンが見つかりません'
-        });
-    };
-F.Tokencheck(req,res,token);
-    if(token){
+    const token=G.Token_Get(req,res);
+    const t=F.Tokencheck(req,res,token);
+    if(t){
         try{
             const tokens=Token.deleteOne({token:token},
                 function(err){
