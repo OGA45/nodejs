e15678b2a9b80df28a3d59592d3079380e1fe1be
OGA45 <50638966+OGA45@users.noreply.github.com>
2020-10-07 07:29:05 +0900

    Delete attendance.js

--- apitest/src/routes/attendance.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const{
-    AttendanceF,
-}=require('../controllers/attendanceC');
-
-router.route('/v1/me/attendance').get(AttendanceF);
-module.exports = router;
