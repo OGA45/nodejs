96a7f0b707829bbe657690784f0605f680c0afe4
OGA45 <50638966+OGA45@users.noreply.github.com>
2020-10-07 07:17:44 +0900

    Delete absenceC.js

--- apitest/src/controllers/absenceC.js
+++
@@ -1,8 +0,0 @@
-require('mongoose');
-const jwt = require('jsonwebtoken');
-const Absence=require('../model/absence_model')
-exports.AbsenceF=async(req,res,next)=>{
-    res.json({
-        "success":false
-    });
-}
