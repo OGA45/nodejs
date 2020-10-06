1a809e33946da02a2d58413a4bafb2cd959f0120
OGA45 <50638966+OGA45@users.noreply.github.com>
2020-10-07 07:29:49 +0900

    Delete signup.js

--- apitest/src/routes/signup.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const{
-    SignupF,
-}=require('../controllers/signupC');
-
-router.route('/v1/auth/signup').post(SignupF);
-module.exports = router;
