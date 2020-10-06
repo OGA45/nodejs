dd647627a61cbad30de5d862da037b5783222a5d
OGA45 <50638966+OGA45@users.noreply.github.com>
2020-10-07 07:25:02 +0900

    Delete attendanceC.js

--- apitest/src/controllers/attendanceC.js
+++
@@ -1,8 +0,0 @@
-require('mongoose');
-const jwt = require('jsonwebtoken');
-//const Oa=require('../model/oa_model')
-exports.AttendanceF=async(req,res,next)=>{
-    res.json({
-        "success":false
-    });
-}
