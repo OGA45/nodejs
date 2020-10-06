d19475076a0bf6c2aa82beace5ebc8dcf212d49e
OGA45 <50638966+OGA45@users.noreply.github.com>
2020-10-07 07:25:18 +0900

    Delete contactC.js

--- apitest/src/controllers/contactC.js
+++
@@ -1,8 +0,0 @@
-require('mongoose');
-const jwt = require('jsonwebtoken');
-const Contact=require('../model/contact_model')
-exports.ContactF=async(req,res,next)=>{
-    res.json({
-        "success":false
-    });
-}
