d51f5433dbe23b09aa43c109970a64ed37f82ed2
OGA45 <50638966+OGA45@users.noreply.github.com>
2020-10-07 07:29:34 +0900

    Delete oa.js

--- apitest/src/routes/oa.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const{
-    OaF,
-}=require('../controllers/oaC');
-
-router.route('/v1/contact/oa').post(OaF);
-module.exports = router;
