6453baed4290661719e9390d50ad19a72cdf36f2
OGA45 <50638966+OGA45@users.noreply.github.com>
2020-10-07 07:25:25 +0900

    Delete infoC.js

--- apitest/src/controllers/infoC.js
+++
@@ -1,8 +0,0 @@
-require('mongoose');
-const jwt = require('jsonwebtoken');
-//const Contact=require('../model/contact_model')
-exports.InfoF=async(req,res,next)=>{
-    res.json({
-        "success":false
-    });
-}
