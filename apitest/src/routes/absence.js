5f368b2ab24a1fb06658fda5d8e45f0c89d41dfc
OGA45 <50638966+OGA45@users.noreply.github.com>
2020-10-07 07:28:57 +0900

    Delete absence.js

--- apitest/src/routes/absence.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const{
-    AbsenceF,
-}=require('../controllers/absenceC');
-
-router.route('/v1/contact/absence').post(AbsenceF);
-module.exports = router;
