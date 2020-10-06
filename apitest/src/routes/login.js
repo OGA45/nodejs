53afd191d90d69587b3bce528da99d954ac7ae7c
OGA45 <50638966+OGA45@users.noreply.github.com>
2020-10-07 07:29:17 +0900

    Delete login.js

--- apitest/src/routes/login.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const{
-    LoginF,
-}=require('../controllers/loginC');
-
-router.route('/v1/auth/login').post(LoginF);
-module.exports = router;
