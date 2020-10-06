2fd96ca132f87e9df4117da58948539f769325b4
OGA45 <50638966+OGA45@users.noreply.github.com>
2020-10-07 07:25:48 +0900

    Delete meC.js

--- apitest/src/controllers/meC.js
+++
@@ -1,8 +0,0 @@
-require('mongoose');
-const jwt = require('jsonwebtoken');
-//const Contact=require('../model/contact_model')
-exports.MeF=async(req,res,next)=>{
-    res.json({
-        "success":false
-    });
-}
